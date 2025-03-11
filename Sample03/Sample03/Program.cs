e922c11739b722308ad221ea2e8f7b882af5d827
Shinsei <murakami.shinsei.zn@gmail.com>
2025-03-11 09:56:55 +0900

    wip

---
+++ Sample03/Sample03/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a;
+double b, c, d;
+
+a = (int) 1.23;
+b = 1.23;
+c = 10.0;
+d = (double)c;
+Console.WriteLine("a={0}, b={1}, c={2}, d={3}", a, b, c, d);
+
+string str1, str2;
+Console.Write("str1");
+str1 = Console.ReadLine();
+Console.Write("str2");
+str2 = Console.ReadLine();
+Console.WriteLine("str1={0}, str2={1}", str1, str2);
+
+const int NUMBER = 123;
+const string str = "ABC";
+Console.WriteLine("number={0}, str={1}", NUMBER, str);
+//NUMBER = 100;
+
+
---
+++ Sample04/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int[] d = { 2, 4, 8 };
+double sum, avg;
+sum = 0;
+for (int i = 0; i < d.Length; i++)
+{
+  Console.Write("{0} ", d[i]);
+  sum += d[i];
+}
+Console.WriteLine();
+avg = sum / d.Length;
+Console.WriteLine("sum={0}, avg={1}", sum, avg);
+
+foreach (int n in d)
+{
+  Console.Write("{0} ", n);
+}
