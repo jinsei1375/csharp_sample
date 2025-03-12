7cc0dd41b3c1cebfca0fedb84b0ce67af66b3f3d
Shinsei <murakami.shinsei.zn@gmail.com>
2025-03-12 09:08:39 +0900

    配列サンプル

--- Sample04/Program.cs
+++ Sample04/Program.cs
@@ -17,3 +17,15 @@ foreach (int n in d)
 {
   Console.Write("{0} ", n);
 }
+Console.WriteLine();
+
+int[,] a = new int[3, 4];
+for (int i = 0; i < 3; i++)
+{
+  for (int j = 0; j < 4; j++)
+  {
+    a[i, j] = i + j;
+    Console.Write("{0} ", a[i, j]);
+  }
+  Console.WriteLine();
+}
