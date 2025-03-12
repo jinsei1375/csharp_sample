c42d9db8704b6451470d76c953c7224a5759336d
Shinsei <murakami.shinsei.zn@gmail.com>
2025-03-12 09:59:21 +0900

    クラス

---
+++ Sample05/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Sample05
+{
+	public class Person
+	{
+        // フィールド
+		private string name = "";
+		private int age = 0;
+
+        // プロパティ定義
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+        public int Age
+        {
+            set { age = value; }
+            get { return age; }
+        }
+
+        public void ShowAgeAndName()
+		{
+			Console.WriteLine("Name: {0}, 年齢: {1}", name, age );
+		}
+		public void SetAgeAndName(string name, int age)
+		{
+			this.name = name;
+			this.age = age;
+		}
+    }
+}
+
---
+++ Sample05/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Sample05
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Person p1 = new Person();
+			Person p2 = new Person();
+			p1.Name = "山田";
+			p1.Age = 10;
+			p2.SetAgeAndName("河田", 20);
+			p1.ShowAgeAndName();
+			p2.ShowAgeAndName();
+        }
+	}
+}
+
